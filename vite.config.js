55c43e0bcdb0deeb1f9c901a6f86b5504c7d7a79
Sandesh Acharya <sandeshacharya2003@gmail.com>
2025-02-20 23:16:28 +0545

    Fix: Update react-scroll import and build config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'react-scroll': require.resolve('react-scroll'),
+'react-scroll': 'react-scroll', 
     },
   },
 });
